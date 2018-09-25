3d13408c295cbddc1cf77c01bd27b10b9abe940b
Garlic OS® <natekean1337@gmail.com>
2018-09-24 19:55:53 -0500

    Revert to finalboss's original bg.js

--- bg.js
+++ bg.js
@@ -1,1 +1,70 @@
-!function(t){function o(t){var o=(t=(t=(t=t.replace(/left|top/g,"0px")).replace(/right|bottom/g,"100%")).replace(/([0-9\.]+)(\s|\)|$)/g,"$1px$2")).match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);return[parseFloat(o[1],10),o[2],parseFloat(o[3],10),o[4]]}if(!document.defaultView||!document.defaultView.getComputedStyle){var n=jQuery.curCSS;jQuery.curCSS=function(t,o,i){if("background-position"===o&&(o="backgroundPosition"),"backgroundPosition"!==o||!t.currentStyle||t.currentStyle[o])return n.apply(this,arguments);var r=t.style;return!i&&r&&r[o]?r[o]:n(t,"backgroundPositionX",i)+" "+n(t,"backgroundPositionY",i)}}var i=t.fn.animate;t.fn.animate=function(t){return"background-position"in t&&(t.backgroundPosition=t["background-position"],delete t["background-position"]),"backgroundPosition"in t&&(t.backgroundPosition="("+t.backgroundPosition),i.apply(this,arguments)},t.fx.step.backgroundPosition=function(n){if(!n.bgPosReady){var i=t.curCSS(n.elem,"backgroundPosition");i||(i="0px 0px"),i=o(i),n.start=[i[0],i[2]];var r=o(n.options.curAnim.backgroundPosition);n.end=[r[0],r[2]],n.unit=[r[1],r[3]],n.bgPosReady=!0}var e=[];e[0]=(n.end[0]-n.start[0])*n.pos+n.start[0]+n.unit[0],e[1]=(n.end[1]-n.start[1])*n.pos+n.start[1]+n.unit[1],n.elem.style.backgroundPosition=e[0]+" "+e[1]}}(jQuery);
+/**
+ * @author Alexander Farkas
+ * v. 1.21
+ */
+
+
+(function($) {
+	if(!document.defaultView || !document.defaultView.getComputedStyle){ // IE6-IE8
+		var oldCurCSS = jQuery.curCSS;
+		jQuery.curCSS = function(elem, name, force){
+			if(name === 'background-position'){
+				name = 'backgroundPosition';
+			}
+			if(name !== 'backgroundPosition' || !elem.currentStyle || elem.currentStyle[ name ]){
+				return oldCurCSS.apply(this, arguments);
+			}
+			var style = elem.style;
+			if ( !force && style && style[ name ] ){
+				return style[ name ];
+			}
+			return oldCurCSS(elem, 'backgroundPositionX', force) +' '+ oldCurCSS(elem, 'backgroundPositionY', force);
+		};
+	}
+	
+	var oldAnim = $.fn.animate;
+	$.fn.animate = function(prop){
+		if('background-position' in prop){
+			prop.backgroundPosition = prop['background-position'];
+			delete prop['background-position'];
+		}
+		if('backgroundPosition' in prop){
+			prop.backgroundPosition = '('+ prop.backgroundPosition;
+		}
+		return oldAnim.apply(this, arguments);
+	};
+	
+	function toArray(strg){
+		strg = strg.replace(/left|top/g,'0px');
+		strg = strg.replace(/right|bottom/g,'100%');
+		strg = strg.replace(/([0-9\.]+)(\s|\)|$)/g,"$1px$2");
+		var res = strg.match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);
+		return [parseFloat(res[1],10),res[2],parseFloat(res[3],10),res[4]];
+	}
+	
+	$.fx.step. backgroundPosition = function(fx) {
+		if (!fx.bgPosReady) {
+			var start = $.curCSS(fx.elem,'backgroundPosition');
+			
+			if(!start){//FF2 no inline-style fallback
+				start = '0px 0px';
+			}
+			
+			start = toArray(start);
+			
+			fx.start = [start[0],start[2]];
+			
+			var end = toArray(fx.options.curAnim.backgroundPosition);
+			fx.end = [end[0],end[2]];
+			
+			fx.unit = [end[1],end[3]];
+			fx.bgPosReady = true;
+		}
+		//return;
+		var nowPosX = [];
+		nowPosX[0] = ((fx.end[0] - fx.start[0]) * fx.pos) + fx.start[0] + fx.unit[0];
+		nowPosX[1] = ((fx.end[1] - fx.start[1]) * fx.pos) + fx.start[1] + fx.unit[1];           
+		fx.elem.style.backgroundPosition = nowPosX[0]+' '+nowPosX[1];
+
+	};
+})(jQuery);
