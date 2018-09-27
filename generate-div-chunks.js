8fb650bc7f2063bdcb79ec6365493a65987556c4
Garlic OS® <natekean1337@gmail.com>
2018-09-27 13:24:04 -0500

    Rename generate-div-chunks.js to generate-bg-rows.js, etc.
    I renamed generate-div-chunks.js to generate-bg-rows.js because I think that makes it more obvious what its purpose is - it generates the rows of divs in the site that offset the rainbow background.
    I also renamed a few variables and edited some comments to reflect this.
    Additionally, I changed the line `for (rowNum = 1; rowNum < bgRowHeight; rowNum++) {` to `for (rowNum = 1; rowNum < 21; rowNum++) {`. I had gotten the 21 in `bgRowHeight` mixed up with the 20 iterations it takes to loop around the class numbers in the CSS. In case you were wondering, the reason it was 21 and not 20 was because the index of the loop started at 1 because the CSS did.

--- generate-div-chunks.js
+++ generate-div-chunks.js
@@ -1,24 +1,24 @@
 /**
-*   Generate Div Chunks
-*   Generate divs in groups of 20 that offset the background's rainbow texture
+*   Generate Bg Rows
+*   Generate bg rows in groups of 20 that offset the background's rainbow texture
 */
-function generateDivChunks(bgHeight) {
+function generateBgRows(bgRowHeight) {
 
     // Set default values
-    if (bgHeight === undefined) { bgHeight = 21; }
+    if (bgRowHeight === undefined) { bgRowHeight = 21; }
 
     // Amount of divs takes to fill the screen at 25% zoom.
     // There are 20 divs for the 20 bg classes in the CSS.
     // bgHeight is how tall each div will be.
     // Rounded up to make sure the last chunk doesn't just barely not stretch all the way to the end.
-    var needed = Math.ceil((screen.height * 5) / (20 * bgHeight));
+    var needed = Math.ceil((screen.height * 5) / (20 * bgRowHeight));
     var made;
     var rowNum;
     
-    for (made = 0; made < needed; made++) {                 // Make [needed] many chunks
-        for (rowNum = 1; rowNum < bgHeight; rowNum++) {     // I'm sorry... index starts at 1 because the div ID's do :(
+    for (made = 0; made < needed; made++) {       // Make [needed] many chunks
+        for (rowNum = 1; rowNum < 21; rowNum++) { // This is a different 21 from the default bgRowHeight. This one is 20 bg rows in the CSS + 1.
             var currentDiv = document.createElement("div"); // Create a div
-            currentDiv.className = "bg row" + rowNum;       // Add its class information with rowNum
+            currentDiv.className = "bg row" + (rowNum + 1); // Give it its class information with rowNum + 1 to match the number it should have from the CSS
             document.body.appendChild(currentDiv);          // Inject it to the bottom of <body>
         }
     }
