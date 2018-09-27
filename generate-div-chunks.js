cea777154bfbee8eadefb625f085143a9c0e9ee2
Garlic OS® <natekean1337@gmail.com>
2018-09-27 13:04:54 -0500

    Fixed default parameters so they actually work now

--- generate-div-chunks.js
+++ generate-div-chunks.js
@@ -2,12 +2,15 @@
 *   Generate Div Chunks
 *   Generate divs in groups of 20 that offset the background's rainbow texture
 */
-function generateDivChunks(bgHeight = 21) {
+function generateDivChunks(bgHeight) {
+
+    // Set default values
+    if (bgHeight === undefined) { bgHeight = 21; }
 
     // Amount of divs takes to fill the screen at 25% zoom.
     // There are 20 divs for the 20 bg classes in the CSS.
     // bgHeight is how tall each div will be.
-    // Always rounded up to [TODO: INSERT EXPLANATION FOR ROUNDING UP HERE]
+    // Rounded up to make sure the last chunk doesn't just barely not stretch all the way to the end.
     var needed = Math.ceil((screen.height * 5) / (20 * bgHeight));
     var made;
     var rowNum;
