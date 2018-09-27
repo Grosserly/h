f7174aba84cef9f1048baab1f414b5238e3af63c
Garlic OS® <natekean1337@gmail.com>
2018-09-27 12:45:12 -0500

    Not self-invoking anymore; add parameters
    The parameters are intended to add increased customizability for any future variations I want to make. They've also got the default values of what they were originally hardcoded to have.
    I renamed x and y to the less generic "xDest" and "yDest" to make it easier at first glance to see how they work to animate the h mask.
    I realized self-invokation is just confusing and doesn't work well without a weird parameter passed into where it's called in the HTML. For this specific script to work correctly while self-invoking, you had to pass "defer" into where it was called in the HTML to make it so it only ran once the site was loaded. Otherwise, it would fail since #h might not be made yet. For example: <script type="text/javascript" src="move-bg-around.js" defer></script>

--- move-bg-around.js
+++ move-bg-around.js
@@ -1,10 +1,16 @@
-(function moveBgAround() {
-    var x    = -1 * (Math.floor(Math.random() * 201));
-    var y    = -1 * (Math.floor(Math.random() * 201));
-    var time =       Math.floor(Math.random() * 1001) + 2000;
+function moveBgAround(xFactor, yFactor, timeFactor) {
+
+    // Set default values
+    if (xFactor === undefined) { xFactor = 201; }
+    if (yFactor === undefined) { yFactor = 201; }
+    if (timeFactor === undefined) { timeFactor = 1001; }
+
+    var xDest = -1 * (Math.floor(Math.random() * 201));
+    var yDest = -1 * (Math.floor(Math.random() * 201));
+    var time  =       Math.floor(Math.random() * 1001) + 2000;
     $("#h").animate({
-        backgroundPosition: "(" + x + "px " + y + "px)"
+        backgroundPosition: "(" + xDest + "px " + yDest + "px)"
     }, time, "swing", function() {
         moveBgAround();
     });
-})();
+}
