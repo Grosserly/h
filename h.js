90f618593e9cec6d67c37cbc27a73a7edd2425ea
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2019-02-04 12:42:28 -0600

    Remove need for jQuery; clean up some areas

--- h.js
+++ h.js
@@ -17,82 +17,72 @@
  *  Result: rainbow text flying around the screen with Brazilian Flower playing in the background
  *  I can't believe I spent one year working on this
  */
-document.addEventListener('DOMContentLoaded', function() {
-
-    var title       = "h";
-    var text        = "h"; // Set false for no text
-    var width       = 14;
-    var height      = 38;
-    var fontSize    = 20;
-    var bgRowHeight = 21;
-    var textDiv     = document.getElementById("text");
-    var audioFile   = "flower.mp3"; // Set false for no audio
-    
-    var srch = window.location.search;
-    
-    if ((srch) && (srch.includes("?p=/"))) {
-	    
-        var query = decodeURIComponent(srch.substring(4)); // Everything after "?p=/"
-        
-        // Add space at the beginning of text with a space in it, so the SVG text gets spaced properly
-	    if ((query.includes(" ")) && (query.substring(0,1) != " ")) { // If text has a space and it's not at the beginning
-            query = " " + query // Add a space
-        }
-        
-        if (query == "r") {
-            title = "r";
-            text  = false;
-
-        } else if (query == "autism") {
-            audioFile = "monty.ogg";
-
-        } else if (query == "dense") {
-            title = "dense";
-            bgRowHeight = 1;
-
-        } else if (query == "nk") {
-            title = "Me";
-            text  = "Nate Kean";
-            width = 97;
-            audioFile = false;
-
-        } else {
-            text  = query;
-            width = textWidth(text);
-        }   
-    }
-    
-    spaIfy();
+document.addEventListener("DOMContentLoaded", function() {
+		
+	let title       = "h";
+	let text        = "h"; // Set false for no text
+	let width       = 14;
+	let height      = 38;
+	let fontSize    = 20;
+	let bgRowHeight = 21;
+	var textDiv     = document.getElementById("text");
+	let audioFile   = "flower.mp3"; // Set false for no audio
 	
-    if (audioFile) {
-        var audioPlayer = new Audio(audioFile);
-	    
-	audioPlayer.addEventListener('ended', function() {
-        this.currentTime = 0;
-        this.play();
-        }, false);
-	    
-        audioPlayer.play();
-    }
+	let srch = window.location.search;
+	
+	if ((srch) && (srch.includes("?p=/"))) {
+		
+		let query = decodeURIComponent(srch.substring(4)); // Everything after "?p=/"
+		
+		// Add space at the beginning of text with a space in it, so the SVG text gets spaced properly
+		if ((query.includes(" ")) && (query.charAt(0) != " ")) { // If text has a space and it's not at the beginning
+			query = " " + query // Add a space
+		}
+		
+		if (query == "r") {
+			title = "r";
+			text  = false;
 
-    document.title = title;
-    
-    generateBgRows(bgRowHeight); // Generate the background divs that offset the rainbow pattern
+		} else if (query == "autism") {
+			audioFile = "monty.ogg";
 
-    if (text) {
-	    
-        var dividedString = divideString(text); // If something goes wrong, this will return False
-        if (!dividedString)
-            dividedString = ["h","h"]; // Fall back to h's if False
+		} else if (query == "dense") {
+			title = "dense";
+			bgRowHeight = 1;
 
-        var textSVG = makeTextSVG(dividedString, width, height, fontSize);
+		} else {
+			title = query;
+			text  = query;
+			width = textWidth(text, "19px Arial");
+		}   
+	}
 
-        var styleInfo = "background-image:url(" + textSVG + ");"; // Set the background for element selected in textDiv 
-        textDiv.setAttribute("style",styleInfo);                  // to the SVG that was just generated 
-        
-        moveTextAround();
-	    
-    }
+	spaIfy();
+
+	document.title = title;
+
+	generateBgRows(bgRowHeight, 42); // Generate the background divs that offset the rainbow pattern
+
+	if (text) {
+
+		let dividedString = divideString(text);
+		dividedString = dividedString || ["h","h"]; // Fall back to h's if divideString returns false
+
+		let textSVG = generateSVGCode(dividedString, width, height, fontSize);
+		textDiv.setAttribute("style", "background-image: url(" + textSVG + ");");
+		
+		moveAround(textDiv);
+	}
+
+	if (audioFile) {
+		const audioPlayer = new Audio(audioFile);
+		audioPlayer.play();
+			
+		audioPlayer.addEventListener("ended", function() {
+			this.currentTime = 0;
+			this.play();
+		}, false);
+	}
 });
 
 
@@ -111,18 +101,18 @@ document.addEventListener('DOMContentLoaded', function() {
  */
 function spaIfy() {
 
-    var l = window.location;
+    let l = window.location;
     
     if (l.search) {
-        var q = {};
-        l.search.slice(1).split('&').forEach(function(v) {
-            var a = v.split('=');
-            q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+        let q = {};
+        l.search.slice(1).split("&").forEach(function(v) {
+            let a = v.split("=");
+            q[a[0]] = a.slice(1).join("=").replace(/~and~/g, "&");
         });
         if (q.p !== undefined) {
             window.history.replaceState(null, null,
-                l.pathname.slice(0, -1) + (q.p || '') +
-                (q.q ? ('?' + q.q) : '') +
+                l.pathname.slice(0, -1) + (q.p || "") +
+                (q.q ? ("?" + q.q) : "") +
                 l.hash
             );
 }}}
@@ -135,12 +125,12 @@ function spaIfy() {
  *  Input:  String
  *  Output: Whole number
 */
-function textWidth(text) {
-    var myCanvas = document.createElement("canvas");
-    var context = myCanvas.getContext("2d");
-    context.font = "19px Arial";
+function textWidth(text, font) {
+    let myCanvas = document.createElement("canvas");
+    let context = myCanvas.getContext("2d");
+    context.font = font;
     
-    var metrics = context.measureText(text);
+    let metrics = context.measureText(text);
     return metrics.width;
 };
 
@@ -148,6 +138,9 @@ function textWidth(text) {
 /**
  *  Divide String
  *  Takes a string, returns a list with two strings
+ *
+ *  I initially wrote this in Python and you can
+ *  tell because it SUCKS lol
  *  
  *  Example:
  *  Input:  "1234"
@@ -167,15 +160,15 @@ function divideString(string) {
 
     const ORIGINAL    = string;
     const LEN         = string.length;
-    var   isOneChar   = false;
-    var   midpoint    = 0;
-    var   firstHalf   = "";
-    var   secondHalf  = "";
-    var   stringList  = [];
+    let   midpoint    = 0;
+    let   firstHalf   = "";
+    let   secondHalf  = "";
+    let   stringList  = [];
 
-    if (LEN == 1) { // Don't bother scrambling the text if it's only one character long
+    if (LEN == 1) // Don't bother scrambling the text if it's only one character long
         return [ORIGINAL,ORIGINAL];
-    } else if (LEN % 2 == 0) { // If the text's length is an even number,
+
+	if (LEN % 2 == 0) { // If the text's length is an even number,
         midpoint = LEN / 2;    // put Midpoint at half its length
     } else { // Text's length is an odd number,
         midpoint = (LEN - 1) / 2; // so put Midpoint 1 to the left of half its length
@@ -205,118 +198,57 @@ function divideString(string) {
  *
  *  This is used for #text's background-image.
  */
-function makeTextSVG(stringList, width, height, fontSize) {
+function generateSVGCode(stringList, width, height, fontSize) {
     return "\"data:image/svg+xml;uft8,<svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='" + width + "' height='" + height + "'><rect width='100%' height='100%' fill='#000' x='0' y='0' fill-opacity='1' mask='url(#mask)' /><mask id='mask'><rect width='100%' height='100%' fill='#fff' x='0' y='0' /><text x='0' y='15' font-family='sans-serif' font-size='" + fontSize + "' fill='#000'>" + stringList[0] + "</text><text x='0' y='34' font-family='sans-serif' font-size='" + fontSize + "' fill='#000'>" + stringList[1] + "</text></mask></svg>\"";
 
 }
 
 
 /**
- *  bg.js
- *  I have no idea how this works
- *  @author Alexander Farkas
- *  v. 1.21
- */
-(function($) {
-
-	var oldAnim = $.fn.animate;
-	$.fn.animate = function(prop){
-		if('background-position' in prop){
-			prop.backgroundPosition = prop['background-position'];
-			delete prop['background-position'];
-		}
-		if('backgroundPosition' in prop){
-			prop.backgroundPosition = '('+ prop.backgroundPosition;
-		}
-		return oldAnim.apply(this, arguments);o
-	};
-	
-	function toArray(strg){
-		strg = strg.replace(/left|top/g,'0px');
-		strg = strg.replace(/right|bottom/g,'100%');
-		strg = strg.replace(/([0-9\.]+)(\s|\)|$)/g,"$1px$2");
-		var res = strg.match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);
-		return [parseFloat(res[1],10),res[2],parseFloat(res[3],10),res[4]];
-	}
-	
-	$.fx.step. backgroundPosition = function(fx) {
-		if (!fx.bgPosReady) {
-			var start = $.curCSS(fx.elem,'backgroundPosition');
-			
-			if(!start){//FF2 no inline-style fallback
-				start = '0px 0px';
-			}
-			
-			start = toArray(start);
-			
-			fx.start = [start[0],start[2]];
-			
-			var end = toArray(fx.options.curAnim.backgroundPosition);
-			fx.end = [end[0],end[2]];
-			
-			fx.unit = [end[1],end[3]];
-			fx.bgPosReady = true;
-		}
-		//return;
-		var nowPosX = [];
-		nowPosX[0] = ((fx.end[0] - fx.start[0]) * fx.pos) + fx.start[0] + fx.unit[0];
-		nowPosX[1] = ((fx.end[1] - fx.start[1]) * fx.pos) + fx.start[1] + fx.unit[1];           
-		fx.elem.style.backgroundPosition = nowPosX[0]+' '+nowPosX[1];
-
-	};
-})(jQuery);
-
-
-/**
  *  Generate BG Rows
- *  Generate bg rows in groups of 20 that offset the background's rainbow texture
+ *  Generate offset BG rows to make the background rainbow diagonal
  *
  *  Example of a div it makes:
- *  <div class="bg row1" style="height:21px"></div>
+ *  <div class="bg" style="height:21px; background-position: -30px;"></div>
  */
-function generateBgRows(bgRowHeight) {
+function generateBgRows(bgRowHeight, offsetAmount) {
 
-    function _makeARow(rowNum, bgRowHeight) {
-        var currentDiv = document.createElement("div"); // Create a div
-        var classInfo = "bg row" + rowNum;
-        var styleInfo = "height:" + bgRowHeight + "px;";
-        currentDiv.setAttribute("class", classInfo);    // Give it its class information
-        currentDiv.setAttribute("style", styleInfo);    // Set its height
-        document.body.appendChild(currentDiv);          // Inject it to the bottom of <body>
+	let masterDiv = document.createElement("div"); // Create a div
+	masterDiv.classList.add("bg");
+	masterDiv.style.height = bgRowHeight + "px;"   // Set its height
+
+    function _makeARow(rowNum) {
+        let newDiv = masterDiv.cloneNode(false); // Create a div
+        newDiv.style.backgroundPosition = rowNum*offsetAmount + "px";
+        document.body.appendChild(newDiv);       // Inject it to the bottom of <body>
     }
 
-    var needed = Math.ceil((screen.height * 5) / bgRowHeight);
-    var rowNum = 1;
+    let needed = Math.ceil((screen.height * 5) / bgRowHeight);
 
-    while (needed > 0) { // Make [needed] many chunks
-        if (rowNum < 21) {
-            _makeARow(rowNum, bgRowHeight);
-            rowNum++;
-        } else {
-            rowNum = 1;
-        }
-        needed--;
+    while (needed > 0) { // Make [needed] many divs to fit the screen
+        _makeARow(needed);
+		needed--;
     }
 }
 
 
 /**
- *  Move Text Around
- *  Moves the text around to random places on the screen
+ *  Move Around
+ *  Slides an element to random places on the screen repeatedly
  *
  *  Pick a random x value, y value, and time value
- *  Move the text to that position on the screen in that amount of time
+ *  Move the text to that position on the screen over that amount of time
  *  Repeat forever
  */
-function moveTextAround() {
+function moveAround(el) {
 
-    var xDest = -1 * (Math.floor(Math.random() * 201));
-    var yDest = -1 * (Math.floor(Math.random() * 201));
-    var time  =       Math.floor(Math.random() * 1001) + 2000;
-    
-    $("#text").animate({
-        backgroundPosition: "(" + xDest + "px " + yDest + "px)"
-    }, time, "swing", function() {
-        moveTextAround();
-    });
+    let xDest   = Math.floor(Math.random() * -201);
+    let yDest   = Math.floor(Math.random() * -201);
+    let time    = Math.floor(Math.random() * 3) + 3; // in seconds
+
+	el.style.transition = "left " + time + "s, top " + time + "s"; // CSS uses seconds
+	el.style.left = xDest + "px";
+	el.style.top  = yDest + "px";
+
+	setTimeout(moveAround, time*1000, el); // JS uses milliseconds
 }
